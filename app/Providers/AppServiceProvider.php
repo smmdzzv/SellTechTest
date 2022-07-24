f4630d5c92ba33c5ee2105dd871716e7da0da2b1
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2022-07-24 22:24:55 +0500

    binding + route for update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\JsonRepository;
+use App\Repositories\Repository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +15,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(Repository::class, JsonRepository::class);
     }
 }
