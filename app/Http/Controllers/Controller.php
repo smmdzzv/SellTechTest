4c6a919b27a5f070720ca446bbe42ebda77903e5
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2022-07-24 16:24:43 +0500

    add controller + config update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Services\SDNService;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    private SDNService $service;
+
+    public function __construct(SDNService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * Updates data
+     *
+     * @return void
+     */
+    public function update(){
+        $this->service->updateData('https://www.treasury.gov/ofac/downloads/sdn.xml');
+    }
+
+    /**
+     * Returns current data state: empty, updating, ok
+     *
+     * @return void
+     */
+    public function state(){
+
+    }
 }
